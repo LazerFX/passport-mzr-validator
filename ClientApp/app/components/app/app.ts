b2ab420ed5fde6a88c03893da0614cb4a7cc84d1
Peter Street <lazerfx@gmail.com>
2017-09-09 11:44:09 +0100

    Navigation Updated Created the basic navigation structure for the site

--- ClientApp/app/components/app/app.ts
+++ ClientApp/app/components/app/app.ts
@@ -7,26 +7,12 @@ export class App {
     configureRouter(config: RouterConfiguration, router: Router) {
         config.title = 'Aurelia';
         config.map([{
-            route: [ '', 'home' ],
+            route: ['', 'home'],
             name: 'home',
             settings: { icon: 'home' },
             moduleId: PLATFORM.moduleName('../home/home'),
             nav: true,
             title: 'Home'
-        }, {
-            route: 'counter',
-            name: 'counter',
-            settings: { icon: 'education' },
-            moduleId: PLATFORM.moduleName('../counter/counter'),
-            nav: true,
-            title: 'Counter'
-        }, {
-            route: 'fetch-data',
-            name: 'fetchdata',
-            settings: { icon: 'th-list' },
-            moduleId: PLATFORM.moduleName('../fetchdata/fetchdata'),
-            nav: true,
-            title: 'Fetch data'
         }, {
             route: 'passport-mzr-validator',
             name: 'passportmzrvalidator',
--- ClientApp/app/components/home/home.ts
+++ ClientApp/app/components/home/home.ts
@@ -1,2 +1,16 @@
+import {inject} from 'aurelia-dependency-injection';
+import {Router} from 'aurelia-router';
+
+@inject(Router)
 export class Home {
+    Router: Router;
+
+    constructor(router: Router) {
+        this.Router = router;
+    }
+
+    mzrRoute():string {
+        let retVal = this.Router.generate("../passport-mzr-validation/passport-mzr-validation");
+        return retVal;
+    }
 }
